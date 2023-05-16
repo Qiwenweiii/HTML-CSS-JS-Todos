bce9437f6943f9bf33697783eda8fbc9d2b21d5c
Qiwenweiii <Qiwenweiii@foxmail.com>
2023-05-16 09:44:47 +0800

    重构分类和todo的数据结构

--- handleLocalStorage.js
+++ handleLocalStorage.js
@@ -1,28 +1,41 @@
-const readTodos = () => {
-  return JSON.parse(localStorage.getItem('todos'));
+const readData = (name) => {
+  return JSON.parse(localStorage.getItem(name));
 };
 
-const writeTodos = (todos) => {
-  localStorage.setItem('todos', JSON.stringify(todos));
+const writeData = (name, data) => {
+  localStorage.setItem(name, JSON.stringify(data));
 };
+
+// 初始化分类的数据结构
+let categories = [
+  {
+    id: 0,
+    title: '默认分类',
+    children: null,
+  },
+];
 
 // 初始化 todos 数据结构
 let todos_data = [
   {
-    category: 'basic',
-    id: '0',
-    title: '默认分类',
-    todos: [
-      { id: '01', todoTitle: 'will do', todoContext: 'something will todo' },
-    ],
+    category_id: 0,
+    id: 0,
+    todos: [],
   },
 ];
 
-// 如果本地有 todos，则读取并保存到 todos_data，否则初始化一个 todos
-const localTodos = readTodos();
+// 读取本地的分类数据
+const localCategory = readData('category');
+if (localCategory) {
+  categories = localCategory;
+} else {
+  writeData('category', categories);
+}
 
+// 如果本地有 todos，则读取并保存到 todos_data，否则初始化一个 todos
+const localTodos = readData('todos');
 if (localTodos) {
   todos_data = localTodos;
 } else {
-  writeTodos(todos_data);
+  writeData('todos', todos_data);
 }
